d4d235fa66117e73a77e6513a815f0993396ac55
natorise <natoriseishot@gmail.com>
2025-02-07 03:47:48 -0500

    add copy on click to queues message

--- src/components/QueuesMessage.tsx
+++ src/components/QueuesMessage.tsx
@@ -1,48 +1,61 @@
-import React, { useContext } from 'react'
-import { QueuesContext, QueueType } from './ContextComponent'
+import React, { useContext } from "react";
+import { QueuesContext, QueueType } from "./ContextComponent";
 
 const QueuesMessage = () => {
-  const { queues } = useContext(QueuesContext)
-  console.log(queues)
+  const { queues } = useContext(QueuesContext);
+  console.log(queues);
+
+  const copyText = (e: React.MouseEvent) => {
+    let divValue = (e.currentTarget as HTMLDivElement).innerText;
+
+    // remove extra \n
+    divValue = divValue.split("\n").filter(x=>x).join("\n")
+
+    navigator.clipboard.writeText(divValue);
+  };
   return (
-    <div>
-      {queues.reduce((a,b)=>a+b.count,0)} total incenses <br /><br />
-      
-      {formatQueues(queues).map(x=>
-        <p>{x}</p>
-      )}
-    </div>
-  )
-}
+    <>
+      <p>{queues.reduce((a, b) => a + b.count, 0)} Total Incenses</p>
+      <br />
+      <div onClick={copyText}>
+        {formatQueues(queues).map((x) => (
+          <p>{x}</p>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default QueuesMessage
+export default QueuesMessage;
 
-const shardPrice = 200
-const incenseShardPrice = 50
-const incensePrice = incenseShardPrice * shardPrice
-
+const shardPrice = 200;
+const incenseShardPrice = 50;
+const incensePrice = incenseShardPrice * shardPrice;
 
 function formatQueues(queues: QueueType[]) {
-  let incenseSum = 0
-  
-  let queueStrings = [] 
-  for(let queue of queues) {
-    queueStrings.push(formatQueue(queue, incenseSum))
-    incenseSum+= queue.count
+  let incenseSum = 0;
+
+  let queueStrings = [];
+  for (let queue of queues) {
+    queueStrings.push(formatQueue(queue, incenseSum));
+    incenseSum += queue.count;
   }
 
-  return queueStrings
+  return queueStrings;
 }
- 
+
 function formatQueue(queue: QueueType, incenseOffset: number) {
-  return `**${queue.name} : ${formatPrice(queue.count * incensePrice)} (${queue.count * incenseShardPrice} shards) - Channels #${incenseOffset+ 1} to #${incenseOffset + queue.count}**`
+  return `**${queue.name} : ${formatPrice(queue.count * incensePrice)} (${
+    queue.count * incenseShardPrice
+  } shards) - Channels #${incenseOffset + 1} to #${
+    incenseOffset + queue.count
+  }**`;
 }
 
 function formatPrice(price: number) {
-  
-  let formatted = String(price)
-  if(formatted.length >= 7) formatted = formatted.slice(0,-6) + "M"
-  else if(formatted.length >= 4) formatted = formatted.slice(0,-3) + "K"
-  
-  return formatted
+  let formatted = String(price);
+  if (formatted.length >= 7) formatted = formatted.slice(0, -6) + "M";
+  else if (formatted.length >= 4) formatted = formatted.slice(0, -3) + "K";
+
+  return formatted;
 }
